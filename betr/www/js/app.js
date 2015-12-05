b2ef1bb62075545016fb8c144a8c1d895e972005
phobos101 <robwilson101@gmail.com>
2015-12-05 16:19:07 +0000

    Adding UI-Router, created routes for landing, swipe and confirmation

--- betr/www/js/app.js
+++ betr/www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('betr', ['ionic'])
-
-.run(function($ionicPlatform) {
+angular
+  .module('betr', ['ionic', 'ui.router'])
+  .config(MainRouter)
+  .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,3 +19,21 @@ angular.module('betr', ['ionic'])
     }
   });
 });
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('landing', {
+    url: '/',
+    templateUrl: 'landing.html',
+  })
+  .state('swipe', {
+    url: '/swipe',
+    templateUrl: 'swipe.html',
+  })
+  .state('confirmation', {
+    url: '/confirmation',
+    templateUrl: 'confirmation.html',
+  });
+
+  $urlRouterProvider.otherwise('/');
+};
