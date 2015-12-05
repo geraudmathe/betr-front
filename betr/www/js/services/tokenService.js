dd107f6c0c647c07986890af97971d927fd65bf0
phobos101 <robwilson101@gmail.com>
2015-12-05 18:12:23 +0000

    Adding functions to set, get and decode a token in the localstorage

---
+++ betr/www/js/services/tokenService.js
@@ -0,0 +1,25 @@
+angular
+  .module('betr')
+  .service('TokenService', TokenService);
+
+TokenService.$inject = ['$window', 'jwthelper'];
+function TokenService($window, jwthelper) {
+  var _this = this;
+
+  _this.saveToken = function(token) {
+    return $window.localStorage.setItem('betr-token', token);
+  };
+
+  _this.getToken = function() {
+    return $window.localStorage.getItem('betr-token');
+  };
+
+  _this.removeToken = function() {
+    return $window.localStorage.removeItem('betr-token');
+  };
+
+  _this.decodeToken = function() {
+    var token = _this.getToken();
+    return token ? jwthelper.decodeToken(token) : {};
+  };
+};
