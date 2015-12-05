4650e8b65a052667094d79491b9de9a3aceb460c
gvialpai <vialpg@gmail.com>
2015-12-05 22:12:09 +0000

    Committing before pushing for Andrew

--- betr/www/js/controllers/eventsController.js
+++ betr/www/js/controllers/eventsController.js
@@ -1,5 +1,5 @@
 angular
-	.module('betr')
+	.module('betr', ['gajus.swing'])
 	.controller('eventsController', EventsController);
 
 EventsController.$inject = ['Event', 'TicketService'];
@@ -16,5 +16,44 @@ function EventsController(Event, TicketService) {
 		_this.all = Event.query();
 		console.log(_this.all);
 	}
+
+	_this.throwout = function (eventName, eventObject) {
+	    console.log('throwout', eventObject);
+	};
+
+	_this.throwoutleft = function (eventName, eventObject) {
+	    console.log('throwoutleft', eventObject);
+	};
+
+	_this.throwoutright = function (eventName, eventObject) {
+	    console.log('throwoutright', eventObject);
+	};
+
+	_this.throwin = function (eventName, eventObject) {
+	    console.log('throwin', eventObject);
+	};
+
+	_this.dragstart = function (eventName, eventObject) {
+	    console.log('dragstart', eventObject);
+	};
+
+	_this.dragmove = function (eventName, eventObject) {
+	    console.log('dragmove', eventObject);
+	};
+
+	_this.dragend = function (eventName, eventObject) {
+	    console.log('dragend', eventObject);
+	};
+
+	_this.options = {
+	    throwOutConfidence: function (offset, elementWidth) {
+	        console.log('throwOutConfidence', offset, elementWidth);
+	        return Math.min(Math.abs(offset) / elementWidth, 1);
+	    },
+	    isThrowOut: function (offset, elementWidth, throwOutConfidence) {
+	        console.log('isThrowOut', offset, elementWidth, throwOutConfidence);
+	        return throwOutConfidence === 1;
+	    }
+	};
 
 }
