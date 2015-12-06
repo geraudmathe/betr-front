de33bf97de55a41f1caa36e122a33cf20640f672
phobos101 <robwilson101@gmail.com>
2015-12-06 11:21:27 +0000

    Demo UI changes

--- betr/www/js/controllers/currentController.js
+++ betr/www/js/controllers/currentController.js
@@ -6,15 +6,4 @@ CurrentController.$inject = ['$stateParams'];
 function CurrentController($stateParams) {
   var _this = this;
   _this.current = $stateParams.current;
-
-  _this.i = 20;
-  (function loop(i) {
-    setTimeout(function() {
-      _this.timeRemaining = i;
-      i--;
-      i > 0 ? loop(i) : i = 20;
-      loop(i);
-    }, 1000);
-  })(0);
-
 };
--- betr/www/js/controllers/eventsController.js
+++ betr/www/js/controllers/eventsController.js
@@ -26,6 +26,16 @@ function EventsController(Event, $state, $firebaseArray) {
   _this.getEvents = function() {
     Event.query(true, updateEvents);
   };
-
+  // 
+  // _this.loop = function(i) {
+  //   setTimeout(function() {
+  //     _this.timeRemaining = i;
+  //     i--;
+  //     i > 0 ? _this.loop(i) : i = 20;
+  //     _this.loop(i);
+  //   }, 1000);
+  // };
+  //
+  // _this.loop(20);
   _this.getEvents();
 };
