4ac6dc346a71183dea93643f47631a986c995706
phobos101 <robwilson101@gmail.com>
2015-12-06 09:28:26 +0000

    Adding bet model

---
+++ betr/www/js/models/bet.js
@@ -0,0 +1,20 @@
+angular
+	.module('betr')
+	.factory('Bet', Bet);
+
+Bet.$inject = ['$resource', 'API'];
+
+function Bet($resource, API) {
+
+	return $resource(
+		API + 'bets',
+		null,
+		{
+			'bet' : {
+				method: 'POST',
+				url: API + 'bets',
+			}
+		}
+	);
+
+}
